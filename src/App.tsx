32f8bdbf8ac2e036fd9076c8bad9e7bae8cd91c4
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-01-18 15:23:17 -0300

    routing prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Sidebar from './components/Sidebar';
 import FrontendWrapper from './components/FrontendWrapper';
 import ListingContainer from './components/ListingContainer';
@@ -6,14 +8,28 @@ const base = 'http://127.0.0.1:3085/api'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Sidebar />
-      <FrontendWrapper>
-        <h2>Testing, yk</h2>
-        <ListingContainer endpoint={base + '/towns'}>
-        </ListingContainer>
-      </FrontendWrapper>
-    </div>
+      <Routes>
+        <Route path="/">
+          <FrontendWrapper>
+            <h2>Testing, yk</h2>
+          </FrontendWrapper>
+        </Route>
+        <Route path="/residents">
+          <h2>Residents</h2>
+          <ListingContainer endpoint={base + '/residents'} />
+        </Route>
+        <Route path="/towns">
+          <h2>Towns</h2>
+          <ListingContainer endpoint={base + '/towns'} />
+        </Route>
+        <Route path="/nations">
+          <h2>Nations</h2>
+          <ListingContainer endpoint={base + '/nations'} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
