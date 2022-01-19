491592ee843b1b2cdf3866ff040d7d62eb6cc8da
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-01-19 14:35:42 -0300

    allow changing api base url.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Sidebar from './components/Sidebar';
@@ -5,32 +7,45 @@ import FrontendWrapper from './components/FrontendWrapper';
 
 import Listing from './views/base/Listing';
 
-const base = 'http://127.0.0.1:3085/api';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
-function App() {
-  return (
-    <>
-      <Sidebar />
-      <FrontendWrapper>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/residents">
-              <Listing name="Residents" dataEndpoint={base + '/residents'} />
-            </Route>
-            <Route path="/towns">
-              <Listing name="Towns" dataEndpoint={base + '/towns'} />
-            </Route>
-            <Route path="/nations">
-              <Listing name="Nations" dataEndpoint={base + '/nations'} />
-            </Route>
-            <Route exact path="/">
-              <h2>Homepage</h2>
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </FrontendWrapper>
-    </>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    bake_cookie('base', 'https://61e82144e32cd90017acc083.mockapi.io/api/');
+  }
+
+  render() {
+    return (
+      <>
+        <Sidebar />
+        <FrontendWrapper>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/residents">
+                <Listing
+                  name="Residents"
+                  dataEndpoint={read_cookie('base') + '/residents'}
+                />
+              </Route>
+              <Route path="/towns">
+                <Listing
+                  name="Towns"
+                  dataEndpoint={read_cookie('base') + '/towns'}
+                />
+              </Route>
+              <Route path="/nations">
+                <Listing
+                  name="Nations"
+                  dataEndpoint={read_cookie('base') + '/nations'}
+                />
+              </Route>
+              <Route exact path="/">
+                <h2>Homepage</h2>
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </FrontendWrapper>
+      </>
+    );
+  }
 }
-
-export default App;
