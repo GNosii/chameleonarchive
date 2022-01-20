21bc20da2a8a3ae4614b2f2235f1e43f7ef4ab5d
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-01-20 14:18:55 -0300

    workbox!

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,42 @@
+import { precacheAndRoute, matchPrecache } from 'workbox-precaching';
+import { setCatchHandler, registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+
+// Manifest
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Offline page handling
+setCatchHandler(async ({ event }) => {
+  if (event.request.destination === 'document') {
+    return matchPrecache('/offline.html');
+  }
+});
+
+// Cache Google Fonts
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.googleapis.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.gstatic.com',
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
 console.debug('Service worker working.');
